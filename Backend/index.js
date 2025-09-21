71763ced5ef672f2f3b39207050cde8c84922cd4
Rohit Kumar <krrohit.2345@gmail.com>
2025-09-22 01:37:54 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -10,7 +10,7 @@ import userRouter from './routes/user.routes.js';
 
 const app=express();
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://nova-next-gen-online-virtual-assistant-mfip.onrender.com",
     credentials:true
 }))
 const port=process.env.PORT || 5000
